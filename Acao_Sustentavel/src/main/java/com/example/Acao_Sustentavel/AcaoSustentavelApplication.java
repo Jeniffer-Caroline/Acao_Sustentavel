c4558259061988ae066d93e37c86b913549af9f8
Jeniffer_caroline <jeniffercaroline530@gmail.com>
2025-04-19 14:34:39 -0300

    Iniciando projeto

---
+++ Acao_Sustentavel/src/main/java/com/example/Acao_Sustentavel/AcaoSustentavelApplication.java
@@ -0,0 +1,13 @@
+package com.example.Acao_Sustentavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaoSustentavelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaoSustentavelApplication.class, args);
+	}
+
+}
---
+++ Acao_Sustentavel/src/test/java/com/example/Acao_Sustentavel/AcaoSustentavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Acao_Sustentavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaoSustentavelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
